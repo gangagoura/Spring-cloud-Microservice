0f84c088961e9aaf6c8ae3201ad8fc3a92163d74
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2021-09-10 11:30:41 -0300

    Add files via upload

---
+++ src/main/java/br/com/dio/productcatalog/model/Product.java
@@ -0,0 +1,34 @@
+package br.com.dio.productcatalog.model;
+
+@Document(indexName = "product", type = "catalog")
+public class Product {
+
+    @Id
+    private Integer id;
+    private String name;
+    private Integer amount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/br/com/dio/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package br.com.dio.productcatalog.repository;
+
+import br.com.dio.productcatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+    List<Product> findByName(String name);
+
+}
